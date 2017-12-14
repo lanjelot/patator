6227e28f283406345ca64acdf01b54df63595662
lanjelot <lanjelot@gmail.com>
2017-12-14 15:23:42 +1000

    Add pypi and vagrant files

---
+++ Vagrantfile
@@ -0,0 +1,50 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+$apt = <<SCRIPT
+export DEBIAN_FRONTEND=noninteractive 
+
+# refresh
+apt-get update -y
+
+# essentials
+apt-get install -y tmux git wget build-essential vim
+
+# requirements.txt deps
+apt-get install -y libcurl4-openssl-dev python3-dev libssl-dev # pycurl
+apt-get install -y ldap-utils # ldapsearch
+apt-get install -y libmysqlclient-dev # mysqlclient-python
+apt-get install -y ike-scan unzip default-jdk
+apt-get install -y libsqlite3-dev libsqlcipher-dev # pysqlcipher
+
+# xfreerdp
+apt-get install -y git-core cmake xsltproc libssl-dev libx11-dev libxext-dev libxinerama-dev libxcursor-dev libxdamage-dev libxv-dev libxkbfile-dev libasound2-dev libcups2-dev libxml2 libxml2-dev libxrandr-dev libxi-dev libgstreamer-plugins-base1.0-dev
+git clone https://github.com/FreeRDP/FreeRDP/ /tmp/FreeRDP && (cd /tmp/FreeRDP && cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_SSE2=ON . && make && sudo make install)
+
+SCRIPT
+
+$patator = <<SCRIPT
+python3 -m venv patatorenv --without-pip
+source patatorenv/bin/activate
+wget --quiet -O - https://bootstrap.pypa.io/get-pip.py | python3
+pip install patator
+
+SCRIPT
+
+Vagrant.configure(2) do |config|
+  config.vm.box = "ubuntu/xenial64"
+  config.vm.box_check_update = false
+ 
+  # prevent TTY error messages
+  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
+
+  config.vm.provision "shell",
+                      inline: $apt,
+                      preserve_order: true,
+                      privileged: true 
+
+  config.vm.provision "shell",
+                      inline: $patator,
+                      preserve_order: true,
+                      privileged: false
+end
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+def parse_requirements(file):
+    required = []
+    with open(file) as f:
+        for req in f.read().splitlines():
+            if not req.strip().startswith('#'):
+                required.append(req)
+    return required
+
+requirements = parse_requirements('requirements.txt')
+long_description = "Patator was written out of frustration from using Hydra, Medusa, Ncrack, Metasploit modules and Nmap NSE scripts for password guessing attacks. I opted for a different approach in order to not create yet another brute-forcing tool and avoid repeating the same shortcomings. Patator is a multi-threaded tool written in Python, that strives to be more reliable and flexible than his fellow predecessors."
+
+setup(
+    name="patator",
+    version="0.7",
+    description="multi-purpose brute-forcer",
+    long_description=long_description,
+    url="https://github.com/lanjelot/patator",
+    author="Sebastien Macke",
+    author_email="pastor@hsc.fr",
+    license="GPLv2",
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Utilities',
+        'Topic :: Security',
+    ],
+
+    keywords="pentest brute force password attack",
+    packages=find_packages(),
+    install_requires=requirements,
+
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+
+    scripts=['patator.py'],
+)

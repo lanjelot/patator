ab7300e88000238c506025726d474be5fbe89d7a
lanjelot <lanjelot@gmail.com>
2025-02-17 20:52:26 +1100

    Fix release.sh

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ TMP_COPY=$(mktemp -d)
 git clone -b master $GIT_REPO $TMP_COPY
 cd $TMP_COPY
 VERSION=$(echo `git tag|sort -V|tail -1`-`git rev-parse --verify HEAD|cut -b -7`)
-sed -i -e "s,^__version__.*$,__version__ = '$VERSION'," patator.py
+sed -i -e "s,^__version__.*$,__version__ = '$VERSION'," src/patator/patator.py
 docker build . -t $DOCKER_IMAGE:$VERSION -t $DOCKER_IMAGE:latest
 
 docker login
